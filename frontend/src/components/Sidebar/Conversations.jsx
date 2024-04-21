18cb0fc4aba3c3485a859bf9e009c095c2e52a84
Anurag <anuragde2004@gmail.com>
2024-04-21 14:13:15 +0530

    UI Completed

---
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+const Converstions = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+ 			<Conversation />
+ 			<Conversation />
+			<Conversation />
+ 			<Conversation />
+ 			<Conversation />
+ 			<Conversation />
+ 		</div>
+  )
+}
+
+export default Converstions
